cb4d45d35268c5e900e933cceb6cab11181eb46b
5hyun <qhslsl@gmail.com>
2023-03-30 00:37:28 +0900

    Feat:  제목 업데이트 mutate

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -30,6 +30,7 @@ export function PostDetail({ post }) {
   );
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) return <h3>Loading!</h3>;
 
@@ -46,14 +47,23 @@ export function PostDetail({ post }) {
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
       <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
       {deleteMutation.isError && (
         <p style={{ color: "red" }}>Error deleting the post</p>
       )}
       {deleteMutation.isLoading && (
         <p style={{ color: "purple" }}>Deleting the post</p>
       )}
-      {deleteMutation.isSuccess && <p style={{ color: "green" }}>Success</p>}
-      <button>Update title</button>
+      {deleteMutation.isSuccess && <p style={{ color: "green" }}>Success</p>}{" "}
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && <p style={{ color: "green" }}>Success</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
